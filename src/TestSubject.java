47c03f002812554b569b3d879f49c1376eb6a568
Nan Zhou <nzhou@ggc.edu>
2021-10-25 13:02:29 -0400

    Add more attributes and their getters and setters

--- src/TestSubject.java
+++ src/TestSubject.java
@@ -11,6 +11,11 @@ public class TestSubject implements Serializable {
 
     private String name;
     private String DoB;
+    private String petName;
+    private String movie;
+    private String character;
+    private String city;
+    private String team;
 
     /**
      * Default constructor
@@ -20,6 +25,26 @@ public class TestSubject implements Serializable {
     public TestSubject(String name, String DoB) {
         this.name = name;
         this.DoB = DoB;
+    }
+
+    /**
+     * All-arg constructor
+     * @param name
+     * @param DoB
+     * @param petName
+     * @param movie
+     * @param character
+     * @param city
+     * @param team
+     */
+    public TestSubject(String name, String DoB, String petName, String movie, String character, String city, String team) {
+        this.name = name;
+        this.DoB = DoB;
+        this.petName = petName;
+        this.movie = movie;
+        this.character = character;
+        this.city = city;
+        this.team = team;
     }
 
     /**
@@ -39,6 +64,46 @@ public class TestSubject implements Serializable {
     }
 
     /**
+     * Setter method for character
+     * @param character
+     */
+    public void setCharacter(String character) {
+        this.character = character;
+    }
+
+    /**
+     * Setter method for city
+     * @param city
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * Setter method for movie
+     * @param movie
+     */
+    public void setMovie(String movie) {
+        this.movie = movie;
+    }
+
+    /**
+     * Setter method for pet name
+     * @param petName
+     */
+    public void setPetName(String petName) {
+        this.petName = petName;
+    }
+
+    /**
+     * Setter method for team
+     * @param team
+     */
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    /**
      * Getter method for name
      * @return
      */
@@ -52,6 +117,46 @@ public class TestSubject implements Serializable {
      */
     public String getDoB() {
         return DoB;
+    }
+
+    /**
+     * Getter method for character
+     * @return character
+     */
+    public String getCharacter() {
+        return character;
+    }
+
+    /**
+     * Getter method for city
+     * @return city
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * Getter method for movie
+     * @return movie
+     */
+    public String getMovie() {
+        return movie;
+    }
+
+    /**
+     * Getter method for pet name
+     * @return pet name
+     */
+    public String getPetName() {
+        return petName;
+    }
+
+    /**
+     * Getter method for team
+     * @return team
+     */
+    public String getTeam() {
+        return team;
     }
 
     /**
