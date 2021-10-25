01de7839320811ce7c6a6b4bd713460fe4f02cb7
Nan Zhou <nzhou@ggc.edu>
2021-10-24 20:27:01 -0400

    Add Javadoc

--- src/CreateProfile.java
+++ src/CreateProfile.java
@@ -6,8 +6,22 @@ import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * Class: CreateProfile
+ * @author Nan Zhou
+ * @version 1.0
+ * Description: this program reads in a number of text files that contain personal information and creates TestSubject objects
+ * that are then stored in an ArrayList. The output of this program is a binary file that contains the ArrayList with TestSubject objects.
+ */
+
 public class CreateProfile {
 
+    /**
+     * Reads a text file containing first names and store them in an ArrayList
+     * @param file
+     * @return an ArrayList of first names
+     * @throws IOException
+     */
     public static ArrayList<String> readFirstName(String file) throws IOException {
         ArrayList<String> firstNames = new ArrayList<>();
         BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
@@ -19,6 +33,12 @@ public class CreateProfile {
         return firstNames;
     }
 
+    /**
+     * Reads a text file containing last names and store them in an ArrayList
+     * @param file
+     * @return an ArrayList of last names
+     * @throws IOException
+     */
     public static ArrayList<String> readLastName(String file) throws IOException {
         ArrayList<String> lastNames = new ArrayList<>();
         BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
@@ -30,6 +50,13 @@ public class CreateProfile {
         return lastNames;
     }
 
+    /**
+     * Reads in two ArrayLists, one containing first names and one containing last names,
+     * one element is pulled randomly from each ArrayList and put together to create a full name
+     * @param firstNames
+     * @param lastNames
+     * @return an ArrayList of full names
+     */
     public static ArrayList<String> createName(ArrayList<String> firstNames, ArrayList<String> lastNames) {
         ArrayList<String> names = new ArrayList<>();
         Random random = new Random();
@@ -46,6 +73,11 @@ public class CreateProfile {
         return names;
     }
 
+    /**
+     * Randomly generates dates between 1970/01/01 and 2000/12/31
+     * @param n
+     * @return an ArrayList of dates
+     */
     public static ArrayList<String> createDoB(int n) {
         ArrayList<String> dates = new ArrayList<>();
         Random random = new Random();
@@ -59,6 +91,11 @@ public class CreateProfile {
         return dates;
     }
 
+    /**
+     * Create TestSubject objects using full names and DoB
+     * @param names
+     * @return an ArrayList of TestSubjects
+     */
     public static ArrayList<TestSubject> createProfile(ArrayList<String> names) {
         ArrayList<TestSubject> list = new ArrayList<>();
         ArrayList<String> dates = createDoB(names.size());
@@ -68,6 +105,11 @@ public class CreateProfile {
         return list;
     }
 
+    /**
+     * Reads in an ArrayList of TestSubjects and write to a binary file
+     * @param list
+     * @throws IOException
+     */
     public static void write(ArrayList<TestSubject> list) throws IOException {
         ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("TestSubjects.bin"));
         for (TestSubject testSubject : list) {
@@ -77,6 +119,11 @@ public class CreateProfile {
         outputStream.close();
     }
 
+    /**
+     * Main method - starting point of program
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
         ArrayList<TestSubject> list = createProfile(createName(readFirstName("fnames.txt"),readLastName("lnames.txt")));
         write(list);
